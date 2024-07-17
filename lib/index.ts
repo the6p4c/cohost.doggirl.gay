1deab5b5df63033af09274344135d477fc7301f3
the6p4c <me@doggirl.gay>
2024-07-17 17:16:55 +0800

    add arrow when posts are hidden

--- lib/index.ts
+++ lib/index.ts
@@ -162,14 +162,16 @@ const actions = [
           // if lastPost is undefined, there are somehow no posts in the thread
           if (!lastPost || !lastPost.parentElement) throw "oof";
 
+          const arrow =
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="mr-1 inline-block w-4"><path fill-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v16.19l6.22-6.22a.75.75 0 111.06 1.06l-7.5 7.5a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 111.06-1.06l6.22 6.22V3a.75.75 0 01.75-.75z" clip-rule="evenodd"></path></svg>';
           const hiddenPosts = document.createElement("button");
           hiddenPosts.className =
             "co-link-button w-full cursor-pointer text-center font-bold";
           hiddenPosts.type = "button";
-          hiddenPosts.innerText =
+          hiddenPosts.innerHTML =
             parentPosts.length == 1
-              ? "1 hidden post"
-              : `${parentPosts.length} hidden posts`;
+              ? `${arrow}1 hidden post`
+              : `${arrow}${parentPosts.length} hidden posts`;
 
           const hairline = document.createElement("hr");
           hairline.className = "co-hairline";
