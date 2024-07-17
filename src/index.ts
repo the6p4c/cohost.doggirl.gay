95f14f42b7bc446e62136afac2ba5741b69eebbb
the6p4c <me@doggirl.gay>
2024-07-17 12:05:32 +0800

    """testing infrastructure"""

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
 import fs from "node:fs/promises";
 import process from "node:process";
 import { BrowserContext, chromium } from "playwright";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 import logger from "./logger";
 import takeScreenshot, { Config } from "./screenshot";
+import tests from "./tests";
 
-async function main() {
-  const args = process.argv.slice(2);
-  if (args.length != 2 && args.length != 3) {
-    console.log("usage: ??? <path to screenshot> <post url> [config JSON]");
-    process.exit(1);
-  }
+function main() {
+  yargs(hideBin(process.argv))
+    .command(
+      "get <path> <url>",
+      "capture a screenshot of a post",
+      (yargs) =>
+        yargs
+          .positional("path", {
+            describe: "path to screenshot",
+            type: "string",
+            demandOption: true,
+          })
+          .positional("url", {
+            describe: "cohost post URL",
+            type: "string",
+            demandOption: true,
+          }),
+      (argv) => commandGet(argv.path, argv.url)
+    )
+    .command(
+      "test [names...]",
+      "run test cases",
+      (yargs) =>
+        yargs.positional("names", {
+          array: true,
+          type: "string",
+          choices: Array.from(Object.keys(tests)),
+          default: [] as string[],
+        }),
+      (argv) => commandTest(argv.names)
+    )
+    .parse();
+}
 
-  const [screenshotPath, postUrl, configString] = args;
-
-  const POST_URL_REGEXP =
-    /^https?:\/\/(www\.)?cohost\.org\/(?<projectHandle>[a-z0-9\-]+)\/post\/(?<slug>.*)/i;
-
-  const match = postUrl.match(POST_URL_REGEXP);
-  if (!match || !match.groups) {
+async function commandGet(path: string, url: string) {
+  const urlParsed = parsePostUrl(url);
+  if (!urlParsed) {
     console.log("error: invalid post url");
     process.exit(1);
   }
-
-  const projectHandle = match.groups.projectHandle;
-  const slug = match.groups.slug;
 
   const defaultConfig: Config = {
     colorScheme: "dark",
     collapseParentPosts: true,
     hideThreadHeader: false,
   };
+  const configString = "";
   const config = {
     ...defaultConfig,
     ...(configString ? JSON.parse(configString) : {}),
@@ -41,14 +65,62 @@ async function main() {
       const screenshot = await takeScreenshot(
         page,
         config,
-        projectHandle,
-        slug
+        urlParsed.projectHandle,
+        urlParsed.slug
       );
-      await fs.writeFile(screenshotPath, screenshot);
+      await fs.writeFile(path, screenshot);
 
-      logger.info(`wrote screenshot to ${screenshotPath}`);
+      logger.info(`wrote screenshot to ${path}`);
     });
   });
+}
+
+async function commandTest(names: string[]) {
+  console.log("running test cases:");
+  for (const name of names) {
+    console.log(`  - ${name}`);
+  }
+
+  await withBrowser()(async (browser) => {
+    for (const name of names) {
+      const testCase = tests[name];
+      console.log(`running test case ${name}`);
+      console.log(`  ${testCase.description}`);
+
+      const parsedUrl = parsePostUrl(testCase.url);
+      if (!parsedUrl) throw "oof";
+
+      await withPage(browser)(async (page) => {
+        const screenshot = await takeScreenshot(
+          page,
+          {
+            colorScheme: "light",
+            collapseParentPosts: false,
+            hideThreadHeader: false,
+          },
+          parsedUrl.projectHandle,
+          parsedUrl.slug
+        );
+
+        await fs.writeFile(`tests/${name}.png`, screenshot);
+      });
+    }
+  });
+}
+
+function parsePostUrl(
+  url: string
+): { projectHandle: string; slug: string } | undefined {
+  const URL_REGEXP =
+    /^https?:\/\/(www\.)?cohost\.org\/(?<projectHandle>[a-z0-9\-]+)\/post\/(?<slug>.*)/i;
+
+  const groups = url.match(URL_REGEXP)?.groups;
+  if (!groups) return undefined;
+
+  return {
+    projectHandle: groups.projectHandle,
+    slug: groups.slug,
+  };
 }
 
 function withBrowser() {
---
+++ src/tests.ts
@@ -0,0 +1,56 @@
+const tests: { [key: string]: TestCase } = {
+  thread1: {
+    description: "thread of one post",
+    url: "https://cohost.org/bark-test/post/6905139-long-thread-post-1",
+  },
+  thread2: {
+    description: "thread of two posts",
+    url: "https://cohost.org/bark-test/post/6905144-long-thread-post",
+  },
+  thread3: {
+    description: "thread of three posts",
+    url: "https://cohost.org/bark-test/post/6905147-and-without-further",
+  },
+
+  "thread1-rebug": {
+    description: "thread of one post, transparent rebug",
+    url: "https://cohost.org/bark-test-2/post/6906926-empty",
+  },
+  "thread2-rebug": {
+    description: "thread of two posts, transparent rebug",
+    url: "https://cohost.org/bark-test-2/post/6906947-empty",
+  },
+  "thread3-rebug": {
+    description: "thread of three posts, transparent rebug",
+    url: "https://cohost.org/bark-test-2/post/6906948-empty",
+  },
+
+  "thread1-rebug-tagged": {
+    description: "one post, rebugged with tags",
+    url: "https://cohost.org/bark-test-2/post/6906685-empty",
+  },
+  "thread2-rebug-tagged": {
+    description: "thread of two posts, rebugged with tags",
+    url: "https://cohost.org/bark-test-2/post/6906502-empty",
+  },
+  "thread3-rebug-tagged": {
+    description: "thread of three posts, rebugged with tags",
+    url: "https://cohost.org/bark-test-2/post/6906506-empty",
+  },
+
+  "expand-cw": {
+    description: "thread of two posts, each with content warnings",
+    url: "https://cohost.org/bark-test/post/3771667-cw-post-2-headline",
+  },
+  "expand-18": {
+    description: "thread of two posts, each marked as 18+ content",
+    url: "https://cohost.org/bark-test/post/3771515-18-post-2-headline",
+  },
+};
+
+export default tests;
+
+export type TestCase = {
+  description: string;
+  url: string;
+};
